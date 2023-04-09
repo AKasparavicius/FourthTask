c0cbedc59c5fbb5b7010b1a2cb9ee9472b6a37bc
AKasparavicius <kasparavicius.ar@gmail.com>
2023-04-09 17:45:45 +0300

    Complete fizzBuzz task

--- app/src/main/java/lt/arnas/fourthtask/MainActivity.kt
+++ app/src/main/java/lt/arnas/fourthtask/MainActivity.kt
@@ -3,6 +3,8 @@ package lt.arnas.fourthtask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import org.w3c.dom.Text
 
@@ -10,8 +12,36 @@ class MainActivity : AppCompatActivity() {
 
     final val TAG = "my_tag"
 
+    lateinit var textListView: ListView
+    lateinit var adapter: ArrayAdapter<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textListView = findViewById(R.id.textListView)
+
+        fizzBuzz()
+    }
+
+    private fun fizzBuzz() {
+        textListView = findViewById(R.id.textListView)
+
+        var array02 = arrayOfNulls<String?>(100)
+
+        for(item in 0 .. 99){
+            if ((item+1) % 3 == 0 && (item+1) % 5 == 0){
+                array02[item] = "FIZZBUZZ"
+            } else if ((item+1) % 5 == 0) {
+                array02[item] = "BUZZ"
+            } else if ((item+1) % 3 == 0) {
+                array02[item] = "FIZZ"
+            } else
+                array02[item] = (item + 1).toString()
+        }
+
+        adapter = ArrayAdapter(
+            this, android.R.layout.simple_expandable_list_item_1, array02
+        )
+        textListView.adapter = adapter
     }
 }
